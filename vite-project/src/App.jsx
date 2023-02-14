880faf465db0b9f1046403165576d6b608d0e9f7
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-02-14 21:49:28 +0800

    installed framer-motion

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const App = () => {
   return (
-    <div className='text-4xl text-teal-800 font-bold'>App</div>
+    <div className='flex p-5 m-5'>
+
+        <motion.div 
+          className='w-[300px] h-[300px] bg-slate-500 mx-auto'
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        />
+          
+        <motion.div 
+          className='w-[300px] h-[300px] bg-teal-500 mx-auto' 
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        />
+
+        <motion.div 
+          className='w-[300px] h-[300px] bg-cyan-500 mx-auto' 
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        />
+
+    </div>
   )
 }
 
